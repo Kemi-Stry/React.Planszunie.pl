704d4d9da37eb8894082444821b8aecbe97e7cbc
Gjenio540 <pawelspisz@gmail.com>
2022-03-22 17:15:22 +0100

    emi

---
+++ backend/src/api/test/routes/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/test',
+    //  handler: 'test.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
