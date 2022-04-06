2d391deb2d26b0d94313db9d2cc4d17f9dea8ffd
Gjenio540 <pawelspisz@gmail.com>
2022-04-06 16:19:26 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import {Route, Switch, Link} from 'react-router-dom';
+import Button from './components/Button';
+import useFetch from './hooks/useFetch';
+
 
 function App() {
+  
+const { loading, error, data } = useFetch('http://localhost:1337/api/games/2')
+console.log(data)
+
+if (loading) return <p>Ładowanko...</p>
+if (error) return <p>Error :(</p>
+
   return (
     <div className="App">
-      <h1>Strona główna</h1>
+        <div>
+          <h1>{data.title}</h1>
+        </div>
     </div>
   );
 }
