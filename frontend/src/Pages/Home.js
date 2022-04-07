529747584f65d27a51994640c64b0eedd250ad34
Gjenio540 <pawelspisz@gmail.com>
2022-04-08 00:37:43 +0200

    Dodanie headera
    Dodanie komponentu header, update podstron

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,6 +1,13 @@
+import Header from "../components/Header"
+
 const Home = () => {
     return(
+        <>
+        <Header />
         <h1>Strona główna 🏠</h1>
+        <img src="https://media.giphy.com/media/iESc3Uc5tqGRy/giphy.gif" alt="" />
+        
+        </>
     )
 }
 export default Home
--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,7 +1,12 @@
+import Header from "../components/Header"
+
 const Login = () =>
 {
     return(
+        <>
+        <Header />
         <h1>Logowanko ❤️</h1>
+        </>
     )
 }
 export default Login
