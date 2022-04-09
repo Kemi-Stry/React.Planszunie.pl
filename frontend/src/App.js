54e261589ebeecb8a90ac6539c1a0612e2f52a36
Gjenio540 <pawelspisz@gmail.com>
2022-04-09 14:33:32 +0200

    Dodanie strony: Regulamin
    Dodanie strony: Regulamin
    Zmiany w App.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import ErrorPage from './Pages/ErrorPage';
+import Regulations from './Pages/Regulations';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/logowanie" element={<Login/>} />
+            <Route path="/regulamin" element={<Regulations/>} />
             <Route path="*" element={<ErrorPage/>}/>
           </Routes>
       </BrowserRouter>
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import Header from "../components/Header"
+import "./Home.css"
 
 const Home = () => {
     return(
